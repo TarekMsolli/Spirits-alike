4a5bf49ed6833b190aa67ede6d6d6e0dd77b3b50
Tarek <tarekmsolli@gmail.com>
2023-05-27 23:09:21 +0100

    added elevator platforms

---
+++ game/elevator.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+@onready var travelDistance = 800
+@onready var intialHeight = position.y
+@onready var targetHeight = position.y - travelDistance
+@onready var up = true
+@onready var elevatorSpeed = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if position.y > targetHeight and up:
+		position.y -= elevatorSpeed * delta
+	elif !up and position.y < intialHeight:
+		position.y += elevatorSpeed * delta
+	else:
+		up = !up
+		
+	pass
